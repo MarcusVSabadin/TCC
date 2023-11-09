baf6bfc9b0fd05403a319dab2d124eebdd0f7949
MarcusVSabadin <84208851+MarcusVSabadin@users.noreply.github.com>
2023-11-08 22:10:19 -0300

    telaUpgrade

--- TCC/Assets/Scripts/Upgrade.cs
+++ TCC/Assets/Scripts/Upgrade.cs
@@ -5,10 +5,41 @@ using UnityEngine.UI;
 
 public class Upgrade : MonoBehaviour
 {
-    [SerializeField] Image telaUpgrades;
+    [SerializeField] GameObject telaUpgrades, upgrade;
+    GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindWithTag("GameController").GetComponent<GameMaster>();
+    }
 
     void OnTriggerEnter2D(Collider2D Collider)
     {
-        telaUpgrades.setActive(true);
+        telaUpgrades.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void UpgradeVida(GameObject upgrade)
+    {
+        gm.UpgradeVida();
+        Time.timeScale = 1f;
+        telaUpgrades.SetActive(false);
+        Destroy(upgrade);
+    }
+
+    public void UpgradeDano(GameObject upgrade)
+    {
+        gm.UpgradeDano();
+        Time.timeScale = 1f;
+        telaUpgrades.SetActive(false);
+        Destroy(upgrade);
+    }
+
+    public void UpgradeVelocida(GameObject upgrade)
+    {
+        gm.UpgradeVelocida();
+        Time.timeScale = 1f;
+        telaUpgrades.SetActive(false);
+        Destroy(upgrade);
     }
 }
